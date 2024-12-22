448a62f899b3e8222a824063868b0022d513b18c
Vicente Cartaxo <juniorcartaxo@gmail.com>
2024-12-22 16:03:48 -0300

    page: Login criado

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router'
+import { Toaster } from 'sonner'
 
 import { AppRoutes } from './routes.tsx'
 
@@ -10,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <HelmetProvider>
       <Helmet titleTemplate="%s | Instituto Elo" />
-
+      <Toaster richColors />
       <AppRoutes />
     </HelmetProvider>
   </BrowserRouter>,
--- src/pages/_layout/auth.tsx
+++ src/pages/_layout/auth.tsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router'
 
+import LandingImage from '@/assets/landing-image.png'
+
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-ieloBlue600">
+        <div className="flex h-full items-center justify-center">
+          <img src={LandingImage} alt="Landing" width={480} />
+        </div>
 
-      <div>
+        <footer className="text-sm text-slate-200">
+          &copy; Instituto Terapêutico Elo - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
