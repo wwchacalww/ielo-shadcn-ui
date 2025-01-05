18654a259e46fa9d652ecc72cc20fe0c09310954
Vicente Cartaxo <juniorcartaxo@gmail.com>
2025-01-05 16:24:20 -0300

    Pg: Dashboard estruturada e 404 not found criada

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
-import './index.css'
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router'
-import { Toaster } from 'sonner'
 
-import { ThemeProvider } from './components/theme/theme-provider.tsx'
-import { AppRoutes } from './routes.tsx'
-
+import { App } from './App'
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <HelmetProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="ielo-theme">
-        <Helmet titleTemplate="%s | Instituto Elo" />
-        <Toaster richColors />
-        <AppRoutes />
-      </ThemeProvider>
-    </HelmetProvider>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
---
+++ src/pages/app/dashboard/payments-chart.tsx
@@ -0,0 +1,92 @@
+import { DollarSign } from 'lucide-react'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const data = [
+  { payment: 'Particular', appointments: 62 },
+  { payment: 'Social', appointments: 90 },
+  { payment: 'Fusex', appointments: 53 },
+  { payment: 'INAS-GDF', appointments: 100 },
+  { payment: 'CBM-DF', appointments: 47 },
+]
+
+const COLORS = [
+  colors.sky['500'],
+  colors.amber['500'],
+  colors.violet['500'],
+  colors.emerald['500'],
+  colors.rose['500'],
+]
+
+export function PaymentsChart() {
+  return (
+    <Card className="col-span-3">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="flex w-full items-center justify-between">
+          <CardTitle className="text-base font-medium">
+            Formas de pagamento
+          </CardTitle>
+          <DollarSign className="h-4 w-4 text-muted-foreground" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart style={{ fontSize: 12 }}>
+            <Pie
+              data={data}
+              dataKey="appointments"
+              name="payment"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].payment.length > 12
+                      ? data[index].payment.substring(0, 12).concat('...')
+                      : data[index].payment}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80"
+                  />
+                )
+              })}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
