93ee99a8e14859925f8c2bb88ae6053481363f6f
Vicente Cartaxo <juniorcartaxo@gmail.com>
2025-02-05 17:21:45 -0300

    Api:Api de alteração de dados do paciente pronto

---
+++ src/api/patient/update-patient.ts
@@ -0,0 +1,28 @@
+import { api } from '@/lib/axios'
+
+interface UpdatePatientBody {
+  id: string
+  name?: string | null
+  birhDate?: string | null
+  email?: string | null
+  cpf?: string | null
+  fone?: string | null
+  address?: string | null
+  payment?: string | null
+  responsible?: string | null
+  parent?: string | null
+  cpfResponsible?: string | null
+  status?: string | null
+}
+
+export async function UpdatePatientBody(data: UpdatePatientBody) {
+  const token = localStorage.getItem('@ielo:token')
+  if (!token) {
+    throw new Error('Token não localizado')
+  }
+  return await api.put('/patient/', data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+}
